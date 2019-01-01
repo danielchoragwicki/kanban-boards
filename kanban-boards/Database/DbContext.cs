049f805a55bbcefe345a2573e0ad6017843e1dff
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2019-01-01 18:35:04 +0100

    Enitity set-up, test model, test  APIController.

---
+++ kanban-boards/Database/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using kanban_boards.Models;
+
+namespace kanban_boards.Database
+{
+    public class DbContext : System.Data.Entity.DbContext
+    {
+        public DbContext()
+        {
+            
+        }
+        public virtual DbSet<Person> Persons { get; set; }
+
+    }
+}
