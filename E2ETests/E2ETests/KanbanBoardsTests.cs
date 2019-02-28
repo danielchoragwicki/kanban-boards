678d33892522e5720cc738053cf6ca935b4ad1b3
Maciej Garncarz <maciej.garncarz@microsoft.wsei.edu.pl>
2019-02-28 23:01:00 +0100

    Setting up E2E solution, sample  test.

---
+++ E2ETests/E2ETests/KanbanBoardsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace E2ETests
+{
+    [TestFixture]
+    public class KanbanBoardsTests
+    {
+        IWebDriver _driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            _driver = new ChromeDriver();
+        }
+        [Test]
+        public void GoogleTest()
+        {
+            string text = "youtube";
+
+            _driver.Navigate().GoToUrl("https://google.pl");
+            var searchTextBox = _driver.FindElement(By.Name("q"));
+            searchTextBox.SendKeys(text + Keys.Enter);
+
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Close();
+        }
+    }
+}
---
+++ E2ETests/E2ETests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("E2ETests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E2ETests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52e17b75-e7d0-4a70-b6e2-29e49e116bf2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
